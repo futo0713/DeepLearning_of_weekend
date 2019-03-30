8aceb7651149a7e8a13dcdf08053724d20c92016
FUTOSHI <36586840+futo0713@users.noreply.github.com>
2019-03-30 11:48:19 -0400

    update

--- MNIST_OPEN.py
+++ MNIST_OPEN.py
@@ -1,17 +1,16 @@
-
 import pickle
 
-save_file = '/Users/tsutsumifutoshishi/Desktop/MNIST_test/mnist.pkl'
+save_file = '/Users/FUTOSHI/Desktop/MNIST_test/mnist.pkl'
 
 with open(save_file, 'rb') as f:
     dataset = pickle.load(f)
 
 train_img,train_label,test_img,test_label = dataset
 
-
+#show image
 import matplotlib.pyplot as plt
 
-n = 2345
+n = 1000
 img = train_img[n].reshape((28, 28))
 
 plt.imshow(img)
