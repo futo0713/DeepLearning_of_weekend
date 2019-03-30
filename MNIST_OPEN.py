fe268249bebd97491e81aae809bf279d1a768b54
FUTOSHI <36586840+futo0713@users.noreply.github.com>
2019-03-30 00:59:48 -0400

    open saved MNIST dataset

---
+++ MNIST_OPEN.py
@@ -0,0 +1,20 @@
+
+import pickle
+
+save_file = '/Users/tsutsumifutoshishi/Desktop/MNIST_test/mnist.pkl'
+
+with open(save_file, 'rb') as f:
+    dataset = pickle.load(f)
+
+train_img,train_label,test_img,test_label = dataset
+
+
+import matplotlib.pyplot as plt
+
+n = 2345
+img = train_img[n].reshape((28, 28))
+
+plt.imshow(img)
+plt.show()
+
+print(train_label[n])
